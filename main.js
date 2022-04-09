c6d441eb5c86134cdc4c03d1d455b81802dcd2d9
Jonathan Flores <85594501+jiflon@users.noreply.github.com>
2022-04-09 11:26:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// Tarea ejercicio 1 dia jueves 7/0422
+// ejercicio 1:
+// pedir 2 números al usuario y mostrar en consola el resultado de aplicar todas las operaciones aritméticas entre esos dos números*/
+
+// ejercicio 2:
+// pedir al usuario dos números y escribir en consola si el primer número es mayor que el segundo
+
+
+//Ejercicio I
+let num1 = prompt("ingresa numero");
+let num2 = prompt ("ingrese numero");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+console.log(num1 + num2);
+
+
+//Ejercicio II 
+let num3 = prompt("ingresa numero");
+let num4 = prompt ("ingrese numero");
+num3 = Number(num1)
+num4 = Number(num2)
+
+if(num3 > num4){
+    console.log("numero mayor")
+} else{ 
+    console.log("numero menor")
+} 
+
+
+
+
